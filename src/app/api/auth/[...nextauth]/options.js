d4f4ce734e342c5d2d67ac60ac1f354761b7996b
RishabhRV4444 <rishabh@arevei.com>
2024-11-16 12:49:59 +0530

    initalise

---
+++ src/app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,57 @@
+
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcryptjs';
+import User from '@/models/adminUser'; // Assuming you’ve set up a User model with MongoDB
+import { connectToDB } from '@/db';
+export const authOptions = {
+        providers: [
+          CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+              username: { label: "Username", type: "username" },
+              email: { label: "Email", type: "email" },
+              password: { label: "Password", type: "password" }
+            },
+            authorize: async (credentials) => {
+              try {
+                console.log(credentials)
+                      await connectToDB();
+                      const user = await User.findOne({username:credentials.username, email: credentials.email });
+                      if(!user ){
+                              throw new Error("Invalid credentials");
+                      }
+
+                      const isMatch = await bcrypt.compare(credentials.password, user.password);
+                      if (!isMatch) {
+                        throw new Error('Invalid credentials');
+                      }
+                      return user;
+              } catch (error) {
+                      throw new Error(error);
+              }
+      
+            }
+          })
+        ],
+        pages:{
+              signIn:'/sign-in'
+        },
+        session: {
+          strategy: "jwt",
+        },
+        callbacks: {
+          async jwt({ token, user }) {
+            if (user) {
+              token.id = user._id;
+              token.role = user.role;
+            }
+            return token;
+          },
+          async session({ session, token }) {
+            session.user.id = token._id;
+            session.user.role = token.role;
+            return session;
+          }
+        },
+        secret: process.env.NEXTAUTH_SECRET
+      }
---
+++ src/middleware.js
@@ -0,0 +1,17 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req){
+        const token = await getToken({req:req});
+        const url = req.nextUrl.pathname;
+        const isPublicPath = url.startsWith('/sign-in')||url.startsWith('/sign-up')||url.startsWith('/verify');
+        if(isPublicPath && token){
+                return NextResponse.redirect(new URL('/dashboard',req.url))
+        }
+        if(!isPublicPath && !token){
+                return NextResponse.redirect(new URL('/sign-in',req.url))
+        }
+}
+export const config = {
+  matcher: ['/sign-in','/sign-up','/','/dashboard/:path*','/verify/:path'],
+};
